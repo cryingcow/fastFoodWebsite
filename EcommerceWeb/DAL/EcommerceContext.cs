67691e5449cfac281831cfaa00d7e0e34f0328eb
cryingcow <duykhang4568@gmail.com>
2022-10-05 14:28:32 +0700

    first commit

---
+++ EcommerceWeb/DAL/EcommerceContext.cs
@@ -0,0 +1,20 @@
+﻿using EcommerceWeb.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace EcommerceWeb.DAL
+{
+    public class EcommerceContext : DbContext
+    {
+        public EcommerceContext() : base("name=EcommerceConnection") { }
+
+        public DbSet<LoaiHang> LoaiHangs { get; set; }
+        public DbSet<MatHang> MatHangs { get; set; }
+        public DbSet<KhachHang> KhachHangs { get; set; }
+        public DbSet<HoaDon> HoaDons { get; set; }
+        public DbSet<ChiTietHoaDon> ChiTietHoaDons { get; set; }
+    }
+}
---
+++ EcommerceWeb/Models/LoaiHang.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace EcommerceWeb.Models
+{
+    public class LoaiHang
+    {
+        public LoaiHang() { }
+        [Key]
+        public int LoaiID { get; set; }
+
+        public string TenLoai { get; set; }
+
+        public ICollection<MatHang> MatHangs { get; set; }
+
+    }
+}
